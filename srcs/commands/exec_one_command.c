e0c8cbc188af7e9be1da4ae1ae362f6bca2ba087
root <root@LAPTOP-UTKVD2O1.localdomain>
2023-04-13 14:11:48 +0200

    cambio splash

--- srcs/commands/exec_one_command.c
+++ srcs/commands/exec_one_command.c
@@ -6,14 +6,16 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 17:09:08 by jutrera-          #+#    #+#             */
-/*   Updated: 2023/04/12 18:51:04 by adpachec         ###   ########.fr       */
+/*   Updated: 2023/04/13 13:46:27 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	ft_dup_fd(t_pipe **pipe_s)
+static void	ft_dup_fd(t_pipe **pipe_s, int *stdout_cpy, int *stdin_cpy)
 {
+	*stdout_cpy = dup(STDOUT_FILENO);
+	*stdin_cpy = dup(STDIN_FILENO);
 	if (pipe_s[0]->fd_in != 0)
 	{
 		dup2(pipe_s[0]->fd_in, STDIN_FILENO);
@@ -26,11 +28,14 @@ static void	ft_dup_fd(t_pipe **pipe_s)
 	}
 }
 
-static void	ft_close_out(int stdout_cpy)
+static void	ft_close_out(int stdout_cpy, int stdin_cpy)
 {
 	close(STDOUT_FILENO);
 	dup2(stdout_cpy, STDOUT_FILENO);
 	close(stdout_cpy);
+	close(STDIN_FILENO);
+	dup2(stdin_cpy, STDIN_FILENO);
+	close(stdin_cpy);
 }
 
 void	exec_one_command(t_token *token_list, t_pipe *pipe_s,
@@ -39,14 +44,14 @@ void	exec_one_command(t_token *token_list, t_pipe *pipe_s,
 	pid_t	pid;
 	char	**split_cmd;
 	int		stdout_cpy;
+	int		stdin_cpy;
 
 	split_cmd = ft_split(pipe_s->cmd[pipe_s->i], ' ');
 	if (split_cmd && ft_is_builtin(split_cmd[0]))
 	{
-		stdout_cpy = dup(STDOUT_FILENO);
-		ft_dup_fd(&pipe_s);
+		ft_dup_fd(&pipe_s, &stdout_cpy, &stdin_cpy);
 		pipe_s->err = exec_builtins(token_list, new_environ, pipe_s->status, 0);
-		ft_close_out(stdout_cpy);
+		ft_close_out(stdout_cpy, stdin_cpy);
 		pipe_s->status = (unsigned char)pipe_s->err;
 	}
 	else if (split_cmd)
@@ -55,7 +60,7 @@ void	exec_one_command(t_token *token_list, t_pipe *pipe_s,
 		if (!pid)
 		{
 			signal(SIGQUIT, &renewprompt);
-			ft_dup_fd(&pipe_s);
+			ft_dup_fd(&pipe_s, &stdout_cpy, &stdin_cpy);
 			pipe_s->file_path = try_access(split_cmd, pipe_s->paths);
 			pipe_s->err = execve(pipe_s->file_path, split_cmd, *new_environ);
 			if (pipe_s->err == -1)
